1f435ddb1e67e5025e0ad109b981462f36de856b
Sobeitnow <65687522+sobeitnow0@users.noreply.github.com>
2025-11-26 09:11:30 -0300

    Refactor AutoMove extension preferences management
    Refactor preferences handling and UI components for the AutoMove extension, introducing new classes for item management and updating the settings structure.

--- prefs.js
+++ prefs.js
@@ -1,79 +1,276 @@
-const { Adw, Gtk, Gio } = imports.gi;
+// SPDX-FileCopyrightText: 2012 Giovanni Campagna <gcampagna@src.gnome.org>
+// SPDX-FileCopyrightText: 2014 Florian Müllner <fmuellner@gnome.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const SCHEMA_ID = 'com.sobeitnow0.AutoNewWorkspace';
+import Adw from 'gi://Adw';
+import Gio from 'gi://Gio';
+import GioUnix from 'gi://GioUnix';
+import GLib from 'gi://GLib';
+import GObject from 'gi://GObject';
+import Gtk from 'gi://Gtk';
 
-class Prefs {
-    constructor() {
-        this._settings = new Gio.Settings({ schema_id: SCHEMA_ID });
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+
+const SETTINGS_KEY = 'application-list';
+
+class NewItem extends GObject.Object {}
+GObject.registerClass(NewItem);
+
+class NewItemModel extends GObject.Object {
+    static [GObject.interfaces] = [Gio.ListModel];
+    static {
+        GObject.registerClass(this);
     }
 
-    buildPrefsWidget() {
-        const prefsWidget = new Adw.PreferencesPage();
-        prefsWidget.set_title("Configuração de Aplicativos");
+    #item = new NewItem();
 
-        const appGroup = new Adw.PreferencesGroup({
-            title: "Aplicativos para Mover Automaticamente",
-            description: "Selecione quais aplicativos terão suas janelas movidas para o primeiro workspace vazio (criando um novo se necessário).",
+    vfunc_get_item_type() {
+        return NewItem;
+    }
+
+    vfunc_get_n_items() {
+        return 1;
+    }
+
+    vfunc_get_item(_pos) {
+        return this.#item;
+    }
+}
+
+class Rule extends GObject.Object {
+    static [GObject.properties] = {
+        'app-info': GObject.ParamSpec.object(
+            'app-info', null, null,
+            GObject.ParamFlags.READWRITE,
+            GioUnix.DesktopAppInfo),
+        // Mantemos a propriedade workspace mas não a exibimos, apenas para compatibilidade interna
+        'workspace': GObject.ParamSpec.uint(
+            'workspace', null, null,
+            GObject.ParamFlags.READWRITE,
+            1, 36, 1),
+    };
+
+    static {
+        GObject.registerClass(this);
+    }
+}
+
+class RulesList extends GObject.Object {
+    static [GObject.interfaces] = [Gio.ListModel];
+    static {
+        GObject.registerClass(this);
+    }
+
+    #settings;
+    #rules = [];
+    #changedId;
+
+    constructor(settings) {
+        super();
+
+        this.#settings = settings;
+        this.#changedId =
+            this.#settings.connect(`changed::${SETTINGS_KEY}`,
+                () => this.#sync());
+        this.#sync();
+    }
+
+    append(appInfo) {
+        const pos = this.#rules.length;
+
+        // Hardcoded workspace 1, pois a extensão ignora o numero agora
+        this.#rules.push(new Rule({appInfo, workspace: 1}));
+        this.#saveRules();
+
+        this.items_changed(pos, 0, 1);
+    }
+
+    remove(id) {
+        const pos = this.#rules.findIndex(r => r.appInfo.get_id() === id);
+        if (pos < 0)
+            return;
+
+        this.#rules.splice(pos, 1);
+        this.#saveRules();
+
+        this.items_changed(pos, 1, 0);
+    }
+
+    #saveRules() {
+        this.#settings.block_signal_handler(this.#changedId);
+        // Salva sempre com :1 para manter compatibilidade com o schema XML
+        this.#settings.set_strv(SETTINGS_KEY,
+            this.#rules.map(r => `${r.app_info.get_id()}:1`));
+        this.#settings.unblock_signal_handler(this.#changedId);
+    }
+
+    #sync() {
+        const removed = this.#rules.length;
+
+        this.#rules = [];
+        for (const stringRule of this.#settings.get_strv(SETTINGS_KEY)) {
+            const [id, _] = stringRule.split(':');
+            const appInfo = GioUnix.DesktopAppInfo.new(id);
+            if (appInfo)
+                this.#rules.push(new Rule({appInfo, workspace: 1}));
+            else
+                log(`Invalid ID ${id}`);
+        }
+        this.items_changed(0, removed, this.#rules.length);
+    }
+
+    vfunc_get_item_type() {
+        return Rule;
+    }
+
+    vfunc_get_n_items() {
+        return this.#rules.length;
+    }
+
+    vfunc_get_item(pos) {
+        return this.#rules[pos] ?? null;
+    }
+}
+
+class AutoMoveSettingsWidget extends Adw.PreferencesGroup {
+    static {
+        GObject.registerClass(this);
+
+        this.install_action('rules.add', null, self => self._addNewRule());
+        this.install_action('rules.remove', 's',
+            (self, name, param) => self._rules.remove(param.unpack()));
+    }
+
+    constructor(settings) {
+        super({
+            title: _('Aplicativos para Novo Workspace'),
+            description: _('Aplicativos selecionados abrirão automaticamente em um novo workspace vazio.'),
         });
 
-        let currentList = this._settings.get_strv('application-list');
-        let apps = Gio.AppInfo.get_all();
+        this._settings = settings;
+        this._rules = new RulesList(this._settings);
 
-        // Ordena os aplicativos pelo nome
-        apps.sort((a, b) => a.get_display_name().localeCompare(b.get_display_name()));
+        const store = new Gio.ListStore({item_type: Gio.ListModel});
+        const listModel = new Gtk.FlattenListModel({model: store});
+        store.append(this._rules);
+        store.append(new NewItemModel());
 
-        for (const app of apps) {
-            if (!app.get_id() || !app.should_show()) {
-                continue;
-            }
+        this._list = new Gtk.ListBox({
+            selection_mode: Gtk.SelectionMode.NONE,
+            css_classes: ['boxed-list'],
+        });
+        this.add(this._list);
 
-            const appId = app.get_id();
-            const appName = app.get_display_name();
+        this._list.bind_model(listModel, item => {
+            return item instanceof NewItem
+                ? new NewRuleRow()
+                : new RuleRow(item);
+        });
+    }
 
-            const toggle = new Gtk.Switch({
-                active: currentList.includes(appId), 
-                valign: Gtk.Align.CENTER,
-            });
-
-            const row = new Adw.ActionRow({
-                title: appName,
-                subtitle: appId,
-            });
-
-            row.add_suffix(toggle);
-            row.set_activatable_widget(toggle);
-
-            // Conexão da mudança de estado
-            toggle.connect('state-set', (sw, state) => {
-                let list = this._settings.get_strv('application-list');
-                
-                if (state) {
-                    if (!list.includes(appId)) {
-                        list.push(appId);
-                    }
-                } else {
-                    list = list.filter(id => id !== appId);
-                }
-                
-                this._settings.set_strv('application-list', list);
-                return false;
-            });
-
-            appGroup.add(row);
-        }
-
-        prefsWidget.add(appGroup);
-        return prefsWidget;
+    _addNewRule() {
+        const dialog = new NewRuleDialog(this.get_root(), this._settings);
+        dialog.connect('response', (dlg, id) => {
+            const appInfo = id === Gtk.ResponseType.OK
+                ? dialog.get_widget().get_app_info() : null;
+            if (appInfo)
+                this._rules.append(appInfo);
+            dialog.destroy();
+        });
+        dialog.show();
     }
 }
 
-function init() {
-    ExtensionUtils.initTranslations();
+class RuleRow extends Adw.ActionRow {
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor(rule) {
+        const {appInfo} = rule;
+        const id = appInfo.get_id();
+
+        super({
+            activatable: false,
+            title: rule.appInfo.get_display_name(),
+        });
+
+        const icon = new Gtk.Image({
+            css_classes: ['icon-dropshadow'],
+            gicon: appInfo.get_icon(),
+            pixel_size: 32,
+        });
+        this.add_prefix(icon);
+
+        // Removemos o seletor de Workspace aqui
+
+        const button = new Gtk.Button({
+            action_name: 'rules.remove',
+            action_target: new GLib.Variant('s', id),
+            icon_name: 'edit-delete-symbolic',
+            has_frame: false,
+            valign: Gtk.Align.CENTER,
+        });
+        this.add_suffix(button);
+    }
 }
 
-function fillPreferencesWindow(window) {
-    const prefs = new Prefs();
-    window.add(prefs.buildPrefsWidget());
+class NewRuleRow extends Gtk.ListBoxRow {
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor() {
+        super({
+            action_name: 'rules.add',
+            child: new Gtk.Image({
+                icon_name: 'list-add-symbolic',
+                pixel_size: 16,
+                margin_top: 12,
+                margin_bottom: 12,
+                margin_start: 12,
+                margin_end: 12,
+            }),
+        });
+        this.update_property(
+            [Gtk.AccessibleProperty.LABEL], [_('Adicionar App')]);
+    }
+}
+
+class NewRuleDialog extends Gtk.AppChooserDialog {
+    static {
+        GObject.registerClass(this);
+    }
+
+    constructor(parent, settings) {
+        super({
+            transient_for: parent,
+            modal: true,
+        });
+
+        this._settings = settings;
+
+        this.get_widget().set({
+            show_all: true,
+            show_other: true, 
+        });
+
+        this.get_widget().connect('application-selected',
+            this._updateSensitivity.bind(this));
+        this._updateSensitivity();
+    }
+
+    _updateSensitivity() {
+        const rules = this._settings.get_strv(SETTINGS_KEY);
+        const appInfo = this.get_widget().get_app_info();
+        this.set_response_sensitive(Gtk.ResponseType.OK,
+            appInfo && !rules.some(i => i.startsWith(appInfo.get_id())));
+    }
+}
+
+export default class AutoMovePrefs extends ExtensionPreferences {
+    getPreferencesWidget() {
+        return new AutoMoveSettingsWidget(this.getSettings());
+    }
 }
